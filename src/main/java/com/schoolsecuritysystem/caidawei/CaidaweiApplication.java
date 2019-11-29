19bb73e4d4c451aa24c53cc1be36e30106feccf6
cdw <caidaweigo@163.com>
2019-11-29 17:02:36 +0800

    迁移项目到Github

---
+++ src/main/java/com/schoolsecuritysystem/caidawei/CaidaweiApplication.java
@@ -0,0 +1,13 @@
+package com.schoolsecuritysystem.caidawei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaidaweiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaidaweiApplication.class, args);
+    }
+
+}
