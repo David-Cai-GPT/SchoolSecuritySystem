0212465fcdd0474c5ff6f35b4720fddc88dd01e8
cdw <caidaweigo@163.com>
2020-04-12 13:07:05 +0800

    车辆管理注释

--- src/main/java/com/schoolSecuritySystem/controller/CarController.java
+++ src/main/java/com/schoolSecuritySystem/controller/CarController.java
@@ -19,18 +19,31 @@ import java.util.List;
 public class CarController {
     @Autowired
     private CarService carService;
+
+    /**
+     *入园车辆记录
+     */
     @ResponseBody
     @PostMapping("/carRecord")
     public boolean CarRecord(@RequestBody CarInfoReq dto) { return carService.carRecord(dto); }
 
+    /**
+     *得到所有车辆信息
+     */
     @ResponseBody
     @PostMapping("/allCarInfo")
     public List<Carrecord> GetAllCarInfo() { return carService.carInfo(); }
 
+    /**
+     *车辆出园记录
+     */
     @ResponseBody
     @PostMapping("/carOutRecord")
     public boolean CarOutRecord(@RequestBody CarInfoReq dto) { return carService.carOutRecord(dto); }
 
+    /**
+     *停车场车位数
+     */
     @ResponseBody
     @PostMapping("/surplusParkNumber")
     public int GetSurplusParkNumber() { return carService.surpluspark(); }
