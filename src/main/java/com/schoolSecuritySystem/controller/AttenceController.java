2763528498fb7cde591c7d94209b1007a707752e
cdw <caidaweigo@163.com>
2020-04-25 14:42:02 +0800

    考情模块功能完善加前端实现

---
+++ src/main/java/com/schoolSecuritySystem/controller/AttenceController.java
@@ -0,0 +1,24 @@
+package com.schoolSecuritySystem.controller;
+
+import com.schoolSecuritySystem.dto.ActivityController.ApplyActivityInfoReq;
+import com.schoolSecuritySystem.dto.AttenceController.AttenceDto;
+import com.schoolSecuritySystem.service.AttenceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/Attence")
+public class AttenceController {
+    @Autowired
+    private AttenceService attenceServiceImpl;
+    @ResponseBody
+    @GetMapping("/notAttenceInfo")
+    public List NotAttenceInfo(@RequestParam("role")int role) { return attenceServiceImpl.notAttenceList(role); }
+
+    @ResponseBody
+    @PostMapping("/personAttence")
+    public boolean PersonAttence(@RequestBody AttenceDto dto) { return attenceServiceImpl.attence(dto); }
+}
