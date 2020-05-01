9caa7530159baeaf78521379b25a2a5f6ab8b203
cdw <caidaweigo@163.com>
2020-05-01 12:49:37 +0800

    视屏监控模块

---
+++ src/main/java/com/schoolSecuritySystem/controller/VideoMonitoringController.java
@@ -0,0 +1,35 @@
+package com.schoolSecuritySystem.controller;
+
+import com.schoolSecuritySystem.util.FileUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@Controller
+public class VideoMonitoringController {
+    @RequestMapping("fileUpload")
+    public String upload(@RequestParam("fileName") MultipartFile file, Map<String, Object> map){
+
+        // 要上传的目标文件存放路径
+        String localPath = "E:/Develop/Files/Photos";
+        // 上传成功或者失败的提示
+        String msg = "";
+
+        if (FileUtils.upload(file, localPath, file.getOriginalFilename())){
+            // 上传成功，给出页面提示
+            msg = "上传成功！";
+        }else {
+            msg = "上传失败！";
+
+        }
+
+        // 显示图片
+        map.put("msg", msg);
+        map.put("fileName", file.getOriginalFilename());
+
+        return "forward:/test";
+    }
+}
